817ee95375ba96bf3482ef68827f8e85cf62312a
Arpita Mehta <arpita.patel02@gmail.com>
2020-05-23 13:48:27 -0700

    added heroku

--- app.js
+++ app.js
@@ -13,24 +13,24 @@ const morganOption = (process.env.NODE_ENV === 'production')
     ? 'tiny'
     : 'common';
 
-app.use(morgan(morganOption))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 app.use('/api/folders', foldersRouter)
 app.use('/api/notes', notesRouter)
 
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
+    app.use((error, req, res, next) => {
+        let response
+        if (process.env.NODE_ENV === 'production') {
+            response = { error: { message: 'server error' } }
+        } else {
+            response = { error }
+        }
     res.status(500).json(response)
 })
+const PORT = process.env.PORT || 8000
 
 
 module.exports = app
